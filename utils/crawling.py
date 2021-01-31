3471ec2f62accd1f46c8e0941d707f8dd1746bc2
seojinsarejj <s19009@naver.com>
2021-01-31 13:55:39 +0900

    [CREATE] Create move to page

--- utils/crawling.py
+++ utils/crawling.py
@@ -9,6 +9,7 @@ from selenium.webdriver import ActionChains
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 from selenium.webdriver.chrome.options import Options
 
@@ -17,24 +18,30 @@ import time
 
 class Crawling:
 
-    url = "https://www.kebhana.com/common/login.do#//HanaBank"
-
+    url = "https://www.kebhana.com/"
+    
     chrome_options = Options()
     chrome_options.add_extension('dncepekefegjiljlfbihljgogephdhph.crx')
     chrome_options.add_experimental_option("excludeSwitches", ["enable-logging"])
     driver = webdriver.Chrome(executable_path='chromedriver',chrome_options=chrome_options)
-    
-    def login(self,my_id,pw):
+    driver.implicitly_wait(10)
 
+    def move_to_page(self,my_id,pw):
+
+        # 로그인 
         self.driver.get(url=self.url)
+        self.driver.find_element_by_xpath('//*[@id="contents"]/div[2]/ul/li[1]/a/span').click()
+        time.sleep(10)
         self.driver.execute_script("document.getElementsByName('userId')[0].value=\'" + my_id + "\'")
-        time.sleep(0.5)
         self.driver.find_element_by_name('pw').click()
         time.sleep(0.5)
         pyautogui.write(pw,interval=0.1)
         self.driver.find_element_by_xpath('//*[@class="btn1 id-login"]').click()
+        time.sleep(5)
+
+        # 계좌 조회 페이지 이동
+        self.driver.switch_to_frame("hanaMainframe")
+        time.sleep(0.1)
+        self.driver.find_element_by_xpath('//*[@id="diyLnb"]/ul/li[3]/a').click()     
         time.sleep(1)
-    
 
-
-        
