89cac849044d781fd84c613e5b9e7709192a28de
seojinsarejj <s19009@naver.com>
2021-02-05 21:53:40 +0900

    [CREATE] Create find larger than usual expenditure service

--- bank_bot.py
+++ bank_bot.py
@@ -5,6 +5,7 @@ from config import token,account
 from service.crawling import Crawling
 from service.embed import Embed
 
+
 my_id = account['id']
 pw = account['pw']
 my_token = token
@@ -27,6 +28,19 @@ async def 거래내역(ctx,*param):
         result = crawling.get_transaction(int(param[0]),int(param[1]))
         await ctx.send(embed=Embed.my_transaction_embed(result))
         
+    except Exception as e:
+        print(e)
+        await ctx.send("오류가 발생하였습니다.")
+
+
+@bot.command()
+async def 이상치(ctx,*param):
+    await ctx.send("잠시만 기다려주세요")
+    try:
+        crawling = Crawling(my_id,pw)
+        result = crawling.get_transaction(int(param[0]),int(param[1]))
+        await ctx.send(embed=Embed.find_larger_than_usual_expenditure(result))
+
     except Exception as e:
         print(e)
         await ctx.send("오류가 발생하였습니다.")
--- service/embed.py
+++ service/embed.py
@@ -1,5 +1,5 @@
 import asyncio, discord
-
+import numpy as np
 class Embed:
 
     def my_transaction_embed(result):
@@ -24,5 +24,28 @@ class Embed:
             name="총 수입",
             value=str(format(plus, ","))
         )
+
+        return embed
+
+    def find_larger_than_usual_expenditure(result):
+
+        embed = discord.Embed(title="평소보다 큰 지출",color=0x00ff56)
+
+        value = [record[3] for record in result if record[3] != 0]
+        q1 , q3 = np.percentile(value, [25,75])
+        IQR = q3 - q1
+        result = [record for record in result if record[3] < (q1 - 1.5 * IQR)]
+
+        if result :
+            for i in range(len(result)):
+                embed.add_field(
+                    name=str(result[i][0]), 
+                    value= result[i][1] + " " + result[i][2]  + " " + str(result[i][3]) + " " + str(result[i][4]) + " " + str(result[i][5]) + " " + result[i][6],
+                    inline=False)
+        else:
+            embed.add_field(
+                name= "평소보다 큰 지출이 없습니다.",
+                value="X"
+            )
 
         return embed
