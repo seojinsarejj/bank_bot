bf62d8e304be8c6aaa276f479246fc20135c32c6
seojinsarejj <s19009@naver.com>
2021-02-05 20:35:33 +0900

    [ADD] Add summarize transaction

--- bank_bot.py
+++ bank_bot.py
@@ -28,12 +28,24 @@ async def 거래내역(ctx,*param):
         
         embed=discord.Embed(title="내 거래 내역", color=0x00ff56)
 
+        minus = plus = 0
+
         for i in range(len(result)):
-            
+            minus += result[i][3]
+            plus += result[i][4]
             embed.add_field(
                 name=str(result[i][0]), 
                 value= result[i][1] + " " + result[i][2]  + " " + str(result[i][3]) + " " + str(result[i][4]) + " " + str(result[i][5]) + " " + result[i][6],
                 inline=False)
+
+        embed.add_field(
+            name="총 지출",
+            value=str(format(minus, ","))
+        )
+        embed.add_field(
+            name="총 수입",
+            value=str(format(plus, ","))
+        )
 
         await ctx.send(embed=embed)
         
