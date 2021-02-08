f022560066e1dd6d18d07c32ba37d79baf658b86
seojinsarejj <s19009@naver.com>
2021-02-08 14:40:03 +0900

    [ADD] Add bot command 잔액통계

--- bank_bot.py
+++ bank_bot.py
@@ -5,6 +5,8 @@ from config import token,account
 from service.crawling import Crawling
 from service.embed import Embed
 
+import pandas as pd
+import matplotlib.pyplot as plt
 
 my_id = account['id']
 pw = account['pw']
@@ -44,5 +46,29 @@ async def 이상치(ctx,*param):
     except Exception as e:
         print(e)
         await ctx.send("오류가 발생하였습니다.")
+
+
+@bot.command()
+async def 잔액통계(ctx,*param):
+    await ctx.send("잠시만 기다려주세요")
+    try:
+        crawling = Crawling(my_id,pw)
+        result = crawling.get_transaction(int(param[0]),int(param[1]))
+        
+        df = pd.DataFrame(result,columns=['date','sortation','content','minus','plus','balance','dealership'])
+        df.date = pd.to_datetime(df.date)
+        df = df.set_index('date')
+        df = df.sort_index(ascending=True)
+        print(df)
+
+        df_graph = df['balance'].plot(title='잔액 통계')
+        fig = df_graph.get_figure()
+        fig.savefig('balance.png')
+
+
+    except Exception as e:
+        print(e)
+        await ctx.send("오류가 발생하였습니다.")
+
 
 bot.run(my_token)
