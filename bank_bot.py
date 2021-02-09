e80d64221be542a1c4ae306debf0f08a0210b815
seojinsarejj <s19009@naver.com>
2021-02-09 15:27:26 +0900

    [ADD] Add handling json file

--- bank_bot.py
+++ bank_bot.py
@@ -1,18 +1,11 @@
 import asyncio, discord
 from discord.ext import commands
 
-from config import token,account
+from config import token
 from service.crawling import Crawling
 from service.statistics import Statistics
 from service.utils import Util
 
-import pandas as pd
-import matplotlib.pyplot as plt
-
-
-
-my_id = account['id']
-pw = account['pw']
 my_token = token
 game = discord.Game("도움말 : !도움말")
 
@@ -23,28 +16,23 @@ bot = commands.Bot(command_prefix='!', status=discord.Status.online,activity=gam
 @bot.event
 async def on_ready():
     print('봇 시작')
-
-
+ 
+ 
 @bot.command()
 async def 거래내역(ctx,*param):
     await ctx.send("잠시만 기다려주세요")
     try:
-        crawling = Crawling(my_id,pw)
-        result = crawling.get_transaction(int(param[0]),int(param[1]))
-        await ctx.send(embed=Statistics.my_transaction_embed(result))
-        
+        await ctx.send(embed=Statistics.my_transaction_embed(param[0],param[1]))  
     except Exception as e:
         print(e)
         await ctx.send("오류가 발생하였습니다.")
 
 
 @bot.command()
-async def 이상치(ctx,*param):
+async def 이상치(ctx):
     await ctx.send("잠시만 기다려주세요")
     try:
-        crawling = Crawling(my_id,pw)
-        result = crawling.get_transaction(int(param[0]),int(param[1]))
-        await ctx.send(embed=Statistics.find_larger_than_usual_expenditure(result))
+        await ctx.send(embed=Statistics.find_larger_than_usual_expenditure())
 
     except Exception as e:
         print(e)
@@ -52,13 +40,10 @@ async def 이상치(ctx,*param):
 
 
 @bot.command()
-async def 잔액통계(ctx,*param):
+async def 잔액통계(ctx):
     await ctx.send("잠시만 기다려주세요")
     try:
-        crawling = Crawling(my_id,pw)
-        result = crawling.get_transaction(int(param[0]),int(param[1]))
-        await ctx.send(file=Statistics.get_balance_graph(result))
-
+        await ctx.send(file=Statistics.get_balance_graph())
     except Exception as e:
         print(e)
         await ctx.send("오류가 발생하였습니다.")
--- service/statistics.py
+++ service/statistics.py
@@ -1,11 +1,24 @@
 import asyncio, discord
 import numpy as np
+import json
+
 from .utils import Util
+from .crawling import Crawling
+
+from config import account
 class Statistics:
 
     # 거래  내역
-    def my_transaction_embed(result):
-         
+    def my_transaction_embed(period,amount):
+
+        my_id = account['id']
+        pw = account['pw']
+
+        crawling = Crawling(my_id,pw)
+        result = crawling.get_transaction(period,amount)
+
+        Util.write_json(period,amount,result)
+
         embed=discord.Embed(title="내 거래 내역", color=0x00ff56)
 
         minus = plus = 0
@@ -31,8 +44,11 @@ class Statistics:
         return embed
 
     # 평소보다 큰 지출
-    def find_larger_than_usual_expenditure(result):
+    def find_larger_than_usual_expenditure():
 
+
+        period,amount,result = Util.read_json()
+        
         embed = discord.Embed(title="평소보다 큰 지출",color=0x00ff56)
 
         value = [record[3] for record in result if record[3] != 0]
@@ -57,7 +73,9 @@ class Statistics:
         return embed
 
     # 잔액 통계
-    def get_balance_graph(result):
+    def get_balance_graph():
+
+        period,amount,result = Util.read_json()
 
         df = Util.result_to_dataframe(result)
         df = df.sort_index(ascending=True)
